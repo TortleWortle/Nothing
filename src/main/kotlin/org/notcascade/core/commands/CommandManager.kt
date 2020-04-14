148086d5b006ec582d32a01f2b6429083cde189f
Bas Kuipers <TortleWortle@users.noreply.github.com>
2020-04-14 17:44:04 +0200

    small cleanup in the command handler.

--- src/main/kotlin/org/notcascade/core/commands/CommandManager.kt
+++ src/main/kotlin/org/notcascade/core/commands/CommandManager.kt
@@ -1,13 +1,11 @@
 package org.notcascade.core.commands
 
 import com.uchuhimo.konf.Config
-import kotlinx.coroutines.flow.merge
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 import org.koin.core.KoinComponent
 import org.koin.core.get
 import org.notcascade.Alias
 import org.notcascade.Route
-
 import org.notcascade.core.dsl.Command
 import org.notcascade.modules.tags.TagManager
 import org.slf4j.Logger
@@ -35,7 +33,7 @@ class CommandManager(val commands: ArrayList<Command>) : KoinComponent {
             }
         }
 
-        alias.forEach {alias ->
+        alias.forEach { alias ->
             aliasRoot.add(alias.route, Pair(alias.route, alias.target))
             alias.alias.forEach {
                 aliasRoot.add(it, Pair(it, alias.target))
@@ -48,48 +46,54 @@ class CommandManager(val commands: ArrayList<Command>) : KoinComponent {
     fun handleCommand(rawMsg: String, event: GuildMessageReceivedEvent) {
         val pair = root.get(rawMsg)
 
-        if (pair == null) {
-
-            val alias = aliasRoot.get(rawMsg)
-
-            if (alias == null) {
-                logger.info(String.format("%s called undefined command: %s", event.author.asTag, rawMsg))
-                handleTag(rawMsg, event)
-                return
-            }
-
-            val (route, target) = alias
-
-            val pair = root.get(target)
-
-            if (pair == null) {
-                logger.error(String.format("Alias called with invalid target %s", target))
-                return
-            }
-
-            val (cmd, originalRoute) = pair
-
-            val aliasArgs = mapArgs(rawMsg, route)
-
-            val args = mapArgs(target, originalRoute)
-
-            val ctx = CommandContext(event, cmd, mergeMaps(args, aliasArgs))
-
+        // if command has been found do your thing
+        if (pair != null) {
+            val (cmd, route) = pair
+            val args = mapArgs(rawMsg, route)
+            val ctx = CommandContext(event, cmd, args)
             execCommand(cmd, ctx)
-
             return
         }
 
-        val (cmd, route) = pair
+        // grab alias pair from rawMsg
+        val alias = aliasRoot.get(rawMsg)
 
-        val args = mapArgs(rawMsg, route)
+        // if there's no alias go handle tags.
+        if (alias == null) {
+            logger.info(String.format("%s called undefined command: %s", event.author.asTag, rawMsg))
+            handleTag(rawMsg, event)
+            return
+        }
+
+        // grab route and targetRoute from alias
+        val (route, targetRoute) = alias
+
+        // fetch original command from targetRoute
+        val originalPair = root.get(targetRoute)
+
+        // if original pair can't be found that means there's an alias with an invalid target.
+        if (originalPair == null) {
+            logger.error(String.format("Alias called with invalid target %s", targetRoute))
+            return
+        }
+
+        val (cmd, originalRoute) = originalPair
+
+        // grab arguments from the message and current route
+        val aliasArgs = mapArgs(rawMsg, route)
+        // grab arguments from the targetRoute and originalRoute
+        // this is so that an alias can specify defaults for an argument
+        val defaultArgs = mapArgs(targetRoute, originalRoute)
+
+        // merge args
+        val args = mergeMaps(defaultArgs, aliasArgs)
 
         val ctx = CommandContext(event, cmd, args)
 
         execCommand(cmd, ctx)
     }
 
-    fun mergeMaps(first : Map<String, String>, second : Map<String, String>) : Map<String, String> {
+    fun mergeMaps(first: Map<String, String>, second: Map<String, String>): Map<String, String> {
         val ret = HashMap<String, String>()
         first.forEach {
             ret.put(it.key, it.value)
